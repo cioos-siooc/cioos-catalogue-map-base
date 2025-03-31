7a32dc6675a4d604ad03a7be8178a2eb3362ddea
turcotteb <turcotteb@ogsl.ca>
2025-03-31 15:13:51 -0400

    pretty good UI left menu collapsing

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@
 import Map from "@/components/Map";
 import { useState} from 'react';
 import Footer from "@/components/Footer";
+import Image from 'next/image';
+
 
 export default function Home() {
   
@@ -10,17 +12,27 @@ export default function Home() {
   const [center] = useState([47.485, -62.48]); // Default center
 
   return (
-    <div className="h-full items-center justify-items-center gap-16 font-[family-name:var(--font-geist-sans)]">
-      <header id="header" className="relative grid text-white bg-[#00adef] w-full p-5 top=0 gap-[12px] flex-wrap">
-        <h1 className="text-[32px] font-bold text-left">OGSL Map</h1>
-        <div>
-          <p className="place-content-center justify-center text-center text-[24px]" >Catalogue cartographique</p>
+    <div className="gap-16 font-[family-name:var(--font-geist-sans)]">
+      <header id="header" className="relative grid grid-cols-3 text-white bg-[#00adef] w-full pt-5 top=0 gap-[12px]">
+        <div className="col-span-1">
+          <a id="headerImg" href="//ogsl.ca">
+            <img id="headerimgsrc" className="pl-10 pt-5" src="components/Images/OGSL_NoTag_White.png"/>
+            <Image 
+              src="/components/Images/OGSL_NoTag_White.png" // Replace with your image path
+              alt="" 
+              width={48} 
+              height={48} 
+              className="rounded-full"
+            />
+          </a>
         </div>
 
+        <div className="col-span-1 col-start-2" >
+          <p className="place-content-center justify-center text-center text-[24px]" >Catalogue cartographique</p>
+        </div>
       </header>
       
       <main className="relative"> 
-       
         <div className="absolute inset-0 z-40">
             <Map center={center}/>
         </div>
@@ -29,9 +41,6 @@ export default function Home() {
           <Footer />
         </div>
       </main>
-
-
-
 
     </div>
   );
