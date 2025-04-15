0533f3b4b8573d3378d8c39f8abe496f9774680e
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-15 14:11:57 +0000

    fix basePath and drop configure-page action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
 
+const github_repository = process.env.GITHUB_REPOSITORY;
+var basePath = ''
+if (github_repository) {
+    basePath = `/${github_repository.split('/')[1]}`;
+}
+
 module.exports = withFlowbiteReact({
     output: 'export', // Enables static export
     images: { unoptimized: true },
+    basePath: basePath,
+    env: {
+        BASE_PATH: basePath,
+    }
 });
