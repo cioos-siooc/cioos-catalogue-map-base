abdef5e8de79daff875639502b0ca73cb588c6b0
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-15 02:58:47 +0000

    add config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const config = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
 
 module.exports = withFlowbiteReact({
     output: 'export', // Enables static export
     images: { unoptimized: true },
+    env: {
+        CONFIG: JSON.stringify(config)
+    },
 });
