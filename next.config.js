8dab30d23e64a802ed0437bfe1e0971adef7180d
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-17 15:33:29 +0000

    generate palettes on the fly from config primary and accent colors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,38 @@
 const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
 const yaml = require('js-yaml');
 const fs = require('fs');
+const chroma = require("chroma-js");
+const path = require("path");
 
 const config = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
+
+// Generate custom theme colors
+const steps = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+function generateTailwindPalette(color, label) {
+  const palette = chroma
+    .scale(["white", color, "black"])
+  const cssVariables = steps
+    .map((step, i) => `--color-${label}-${step}: ${palette(step / 1000).hex()};`)
+    .join("\n");
+
+  return cssVariables;
+}
+
+function GenerateTheme() {
+    const primaryColor = config.theme.primary_color;
+    const accentColor = config.theme.accent_color;
+    
+    const cssContent = `@theme {
+        ${generateTailwindPalette(primaryColor, 'grey')}
+        ${generateTailwindPalette(accentColor, 'accent')}
+        }\n`;
+    
+    const outputPath = path.join(__dirname, "../app/theme.css");
+    
+    fs.writeFileSync(outputPath, cssContent);
+    console.log("Color custom palette with primary=" + primaryColor + " and accent=" + accentColor);
+}
+GenerateTheme();
 
 const github_repository = process.env.GITHUB_REPOSITORY;
 var basePath = ''
