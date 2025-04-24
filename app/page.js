8242ed6a60d7908c48c1b149ac8a6084ed7ada51
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-24 19:15:00 -0400

    define lang in useState

--- app/page.js
+++ app/page.js
@@ -4,13 +4,14 @@ import { useState} from 'react';
 import LeftMenu from "@/components/LeftMenu";
 import dynamic from "next/dynamic";
 import ModalAPropos from "@/components/ModalAPropos";
-
+import config from "./config"
 
 export default function Home() {
   
   const [center] = useState([47.485, -62.48]); // Default center
   const [bounds, setBounds] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [lang, setLang] = useState(config.default_language);
 
   const Map = dynamic(() => import('@/components/Map'), { ssr: false });
 
@@ -33,7 +34,7 @@ const handleCloseModal = () => {
     <div className="gap-16 font-[family-name:var(--font-geist-sans)]">
      
       <main>
-        <LeftMenu onInfoClick={onInfoClick} onItemClick={handleListItemClick}/>        
+        <LeftMenu onInfoClick={onInfoClick} onItemClick={handleListItemClick} lang={lang} setLang={setLang} />        
         <div className="md:ml-64">
           <Map center={center} bounds={bounds} />
         </div>
