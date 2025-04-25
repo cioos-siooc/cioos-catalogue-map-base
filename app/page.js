5bd0ed255af1535e2e4128a93652879ed30aac36
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-25 12:21:01 -0400

    run eslint and prettier to fix issues

--- app/page.js
+++ app/page.js
@@ -1,20 +1,18 @@
-'use client';
+"use client";
 
-import { useState} from 'react';
+import { useState } from "react";
 import { Sidebar, TopBanner } from "@/components/LeftMenu";
 import dynamic from "next/dynamic";
 import ModalAPropos from "@/components/ModalAPropos";
-import config from "./config"
+import config from "./config";
 
 export default function Home() {
-  
   const [center] = useState([47.485, -62.48]); // Default center
   const [bounds, setBounds] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [lang, setLang] = useState(config.default_language);
 
-  const Map = dynamic(() => import('@/components/Map'), { ssr: false });
-
+  const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
   const handleListItemClick = (selectedItem) => {
     setBounds(selectedItem.spatial);
@@ -23,12 +21,10 @@ export default function Home() {
   const onInfoClick = () => {
     console.log("INFO CLICKED");
     setShowModal(true);
-};
-const handleCloseModal = () => {
+  };
+  const handleCloseModal = () => {
     setShowModal(false);
-};
-
-
+  };
 
   return (
     <div className="h-screen flex flex-col">
@@ -37,7 +33,12 @@ const handleCloseModal = () => {
       </header>
       <div className="h-screen flex flex-1">
         <aside className="hidden md:block w-sm h-screen overflow-auto">
-          <Sidebar onInfoClick={onInfoClick} onItemClick={handleListItemClick} lang={lang} setLang={setLang} />
+          <Sidebar
+            onInfoClick={onInfoClick}
+            onItemClick={handleListItemClick}
+            lang={lang}
+            setLang={setLang}
+          />
         </aside>
         <main className="z-20 flex-1 h-full w-full">
           <Map center={center} bounds={bounds} />
--- components/Map.js
+++ components/Map.js
@@ -1,65 +1,56 @@
-'use client';
+"use client";
 
-import 'leaflet/dist/leaflet.css';
-import '../app/globals.css';
+import "leaflet/dist/leaflet.css";
+import "../app/globals.css";
 import { MapContainer, Polygon, TileLayer, useMap } from "react-leaflet";
-import L from 'leaflet';
-
-
+import L from "leaflet";
 
 const FitBounds = ({ bounds }) => {
+  const map = useMap();
+  if (bounds) {
+    ClearMap({ map });
+    var polygon = L.geoJSON(bounds, { color: "red" }).addTo(map);
 
-    const map = useMap();
-    if (bounds) {
-        ClearMap({map});
-        var polygon = L.geoJSON(bounds, {color: 'red'}).addTo(map);
-        
-        map.fitBounds(polygon.getBounds(), {
-            animate: true,
-            padding: [50, 50],
-            maxZoom: 10
-        });
-    }
-
-    return null;
-  };
-
-  const ClearMap = ({map}) => {
-    map.eachLayer((layer) => {
-        if (layer instanceof L.Polygon) {
-            map.removeLayer(layer);
-        }
-        if (layer instanceof L.Marker) {
-            map.removeLayer(layer);
-        }
-
+    map.fitBounds(polygon.getBounds(), {
+      animate: true,
+      padding: [50, 50],
+      maxZoom: 10,
     });
+  }
 
+  return null;
+};
 
-    return null;
-  };
+const ClearMap = ({ map }) => {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Polygon) {
+      map.removeLayer(layer);
+    }
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
+  });
 
+  return null;
+};
 
-function Map({center,bounds}){
-
-    return(
-        <div id="container" className="h-full w-full">
-            {center && (
-            
-                <MapContainer
-                className="h-full w-full"
-                center={center} zoom={6} 
-                scrollWheelZoom={true}
-                boundsOptions={{ padding: [1, 1] }}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {bounds && <FitBounds bounds={bounds} />}
-            </MapContainer>
-            )}
-        </div>
-
-    )
+function Map({ center, bounds }) {
+  return (
+    <div id="container" className="h-full w-full">
+      {center && (
+        <MapContainer
+          className="h-full w-full"
+          center={center}
+          zoom={6}
+          scrollWheelZoom={true}
+          boundsOptions={{ padding: [1, 1] }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {bounds && <FitBounds bounds={bounds} />}
+        </MapContainer>
+      )}
+    </div>
+  );
 }
 
 export default Map;
