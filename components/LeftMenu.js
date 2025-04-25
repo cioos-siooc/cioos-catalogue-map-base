a2823ef127eb31e7b403a713cfd3c5ba17d5e07f
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-25 12:56:52 -0400

    bring back filter

--- components/LeftMenu.js
+++ components/LeftMenu.js
@@ -20,7 +20,7 @@ export function Sidebar({ onInfoClick, onItemClick, lang, setLang }) {
   const [selectedValue, setSelectedValue] = useState("");
   const [fetchURLFilter, setFetchURLFilter] = useState(config.base_query);
 
-  const t = getLocale(lang);
+    const t = getLocale(lang);
 
   const basePath = process.env.BASE_PATH || "";
 
@@ -183,54 +183,9 @@ export function Sidebar({ onInfoClick, onItemClick, lang, setLang }) {
                             </svg>
                         </button>
                     </div>
-                    
-                    
-                    <ul className="space-y-2 font-medium">
-                        <li>
-
-                            <form className="max-w-lg mx-auto">
-                                <label>Filtrer par</label>
-                                <div className="flex">
-                                    <select
-                                        id="selectCategory"
-                                        className="py-2 pl-3 text-sm text-gray-700 dark:text-gray-200 bg-gray-50 border 
-                                        border-gray-300 rounded-l-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                                        aria-labelledby="dropdown-button"
-                                        onChange={handleSelectChange} >
-                                        <option value="search">Recherche</option>
-                                        <option value="organisation">Organisation</option>
-                                        <option value="project">Projet</option>
-                                        <option value="eov">EOV</option>
-                                    </select>
-                                    <div className="relative w-full">
-                                        <input
-                                            type="search"
-                                            id="search-dropdown"
-                                            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                                            placeholder="Appliquer filtre"
-                                            required
-                                            onChange= {handleChange}
-                                            value={inputValue}
-                                        />
-                                        <button
-                                            type="button"
-                                            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                            onClick={handleFilterClick}
-                                        >
-                                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
-                                            </svg>
-                                            <span className="sr-only">Add filter item</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-
-                        </li>
-                    </ul>
+                    <FilterSection />
                     <div id="badgesSection" className="mt-3 mb-3 relative w-full" >
                         {badges.map(badge => (
-                            
                             <Badge key={badge.index} index={badge.index} label={badge.nom} onRemoveClick={onRemoveClick} />
 
                         ))}
