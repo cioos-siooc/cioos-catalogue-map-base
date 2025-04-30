e4372218d4e339c3838b1c4387ff83f6943cc4d4
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-30 16:32:43 -0400

    add banner back

--- components/LeftMenu.js
+++ components/LeftMenu.js
@@ -340,3 +340,57 @@ export function Sidebar({ onInfoClick, onItemClick, lang, setLang }) {
     </div>
   );
 }
+
+export const TopBanner = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <button
+      id="sidebar-toggle"
+      data-drawer-target="logo-sidebar"
+      data-drawer-toggle="logo-sidebar"
+      aria-controls="logo-sidebar"
+      type="button"
+      onClick={toggleSidebar}
+      className="w-screen flex justify-between items-center p-2 text-sm bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    >
+      <div className="flex items-center ps-2.5">
+        <a className="me-3">
+          <Image
+            src="/Images/OGSL_NoTag.png"
+            className="h-auto dark:hidden"
+            alt="OGSL Logo"
+            height={0}
+            width={120}
+          />
+          <Image
+            src="/Images/OGSL_NoTag_White.png"
+            className="h-auto hidden dark:block"
+            alt="OGSL Logo"
+            height={0}
+            width={129}
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
+            Carte de l&apos;OGSL
+          </span>
+        </a>
+        <span className="sr-only">Open sidebar</span>
+      </div>
+      <svg
+        className="w-6 h-6"
+        aria-hidden="true"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          clipRule="evenodd"
+          fillRule="evenodd"
+          d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+        ></path>
+      </svg>
+    </button>
+  );
+};
