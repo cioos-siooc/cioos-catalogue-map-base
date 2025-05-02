c1bcdd771d41b51da6e599115c4aa668ba8aa918
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-05-02 13:57:23 -0400

    add-spinner

--- components/ItemsList.js
+++ components/ItemsList.js
@@ -1,10 +1,23 @@
 "use client";
+import { ImSpinner2 } from "react-icons/im";
+import { getLocale } from "@/app/get-locale";
+
 export default function ItemsList({
   itemsList,
   onItemClick,
   onItemDoubleClick,
   lang,
 }) {
+  const t = getLocale(lang);
+  if (itemsList.length === 0) {
+    return (
+      <div className="flex justify-center items-center h-full text-gray-500 dark:text-gray-400">
+        <p>{t.load_datasets} ...</p>
+        <br />
+        <ImSpinner2 className="animate-spin inline-block w-4 h-4 text-gray-500 dark:text-gray-400" />
+      </div>
+    );
+  }
   return (
     <div>
       <div className="grid grid-flow-row z-50">
