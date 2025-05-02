439c797bb2dbebfb5b30029b46a49a57952f23bb
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-05-02 14:42:14 -0400

    add overflow to description section

--- components/DatasetDetails.js
+++ components/DatasetDetails.js
@@ -9,7 +9,6 @@ import { getLocale } from "@/app/get-locale.js";
 
 export function DatasetDetails({ dataSetInfo, lang }) {
   const { isDrawerOpen, closeDrawer } = useContext(DrawerContext);
-  console.log("DatasetDetails :: ", dataSetInfo);
   const t = getLocale(lang);
 
   return (
@@ -21,37 +20,38 @@ export function DatasetDetails({ dataSetInfo, lang }) {
         backdrop={false}
         className="bg-primary-50/75 dark:bg-primary-900/75 h-screen w-sm"
       >
-        <DrawerHeader titleIcon={() => <></>} />
-        <DrawerItems>
-          {dataSetInfo && dataSetInfo.organization ? (
-            <Image
-              className="rounded-sm w-auto w-max-sm max-h-40 bg-white p-1"
-              src={
-                dataSetInfo &&
-                dataSetInfo.organization.image_url_translated[lang]
-              }
-              alt="Organization Logo"
-              width={0}
-              height={40}
-            />
-          ) : (
-            <p>No image available</p>
-          )}
+        <DrawerItems className="flex flex-col h-full">
+          <div id="top" className="flex-shrink-0">
+            {dataSetInfo && dataSetInfo.organization ? (
+              <Image
+                className="rounded-sm w-auto max-h-40 bg-white p-1"
+                src={
+                  dataSetInfo &&
+                  dataSetInfo.organization.image_url_translated[lang]
+                }
+                alt="Organization Logo"
+                width={0}
+                height={40}
+              />
+            ) : (
+              <p>No image available</p>
+            )}
 
-          <div className="flex flex-col gap-1">
-            <h4 className="mt-2 font-bold">
-              {dataSetInfo && dataSetInfo.title_translated[lang]}
-            </h4>
-            <hr className="border-gray-800 dark:border-gray-200" />
-            <p className="text-xs">
-              {t.license}: {dataSetInfo && dataSetInfo.license_title}
-            </p>
-
-            <div className="text-sm overflow-auto mt-4 mb-4">
-              {dataSetInfo && dataSetInfo.notes_translated[lang]}
+            <div className="flex flex-col gap-1 mt-4">
+              <h4 className="font-bold">
+                {dataSetInfo && dataSetInfo.title_translated[lang]}
+              </h4>
+              <hr className="border-gray-800 dark:border-gray-200" />
+              <p className="text-xs">
+                {t.license}: {dataSetInfo && dataSetInfo.license_title}
+              </p>
             </div>
-            <Citation dataSetInfo={dataSetInfo} lang={lang} />
           </div>
+
+          <div className="relative flex-grow overflow-y-auto mt-4 mb-4 text-sm">
+            {dataSetInfo && dataSetInfo.notes_translated[lang]}
+          </div>
+          <Citation dataSetInfo={dataSetInfo} lang={lang} />
         </DrawerItems>
       </Drawer>
     </>
