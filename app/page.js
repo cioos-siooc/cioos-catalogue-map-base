71c54aa2e8f0853c30954b3e7aadbefb2fad87d5
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-05-05 15:51:37 -0400

    add useEffect missing depenancies

--- app/page.js
+++ app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
       }
     };
     fetchData();
-  }, [fetchURLFilter]);
+  }, [fetchURLFilter, initFetchURL, initFetchResults]);
 
   function initFetchResults(awaitRes) {
     setFilteredItems(awaitRes.result.results);
