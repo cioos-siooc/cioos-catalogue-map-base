b54a781489b420585045e6b1f846d9c5e2525285
turcotteb <turcotteb@ogsl.ca>
2025-05-08 18:40:53 +0000

    optional logo is now possible

--- components/Logo.js
+++ components/Logo.js
@@ -3,24 +3,31 @@ import config from "@/app/config.js";
 
 const basePath = process.env.BASE_PATH || "";
 const Logo = (lang) => {
+  let language = lang["lang"];
+  const logoLightURL = config?.bottom_logo?.light[language];
+  const logoDarkURL = config?.bottom_logo?.dark[language];
+
   return (
     <div>
       <div className="relative ml-15 w-32 h-32 md:w-16 md:h-16 lg:w-48 lg:h-16">
-        <Image
-          src={`${basePath}${config.bottom_logo[lang["lang"]]}`}
-          className="h-auto dark:hidden"
-          alt="CIOOS Logo"
-          fill="true"
-          object-fit="contain"
-        />
-
-        <Image
-          src={`${basePath}${config.bottom_logo_dark[lang["lang"]]}`}
-          className="h-auto hidden dark:block"
-          alt="CIOOS Logo dark"
-          fill="true"
-          object-fit="contain"
-        />
+        {config.bottom_logo && (
+          <>
+            <Image
+              src={`${basePath}${logoLightURL}`}
+              className="h-auto dark:hidden"
+              alt="CIOOS Logo"
+              fill="true"
+              object-fit="contain"
+            />
+            <Image
+              src={`${basePath}${logoDarkURL}`}
+              className="h-auto hidden dark:block"
+              alt="CIOOS Logo dark"
+              fill="true"
+              object-fit="contain"
+            />
+          </>
+        )}
       </div>
     </div>
   );
