2d28d1218551b459ecf43be803ec8d54fd49e068
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-05-09 12:07:47 -0400

    add comments

--- components/Logo.js
+++ components/Logo.js
@@ -2,6 +2,17 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import config from "@/app/config.js";
 
+/**
+ * A React component that renders a logo based on the provided language, mode, and available logos.
+ * It supports automatic detection of dark mode or a forced mode, and selects the appropriate logo accordingly.
+ *
+ * @param {Object[]} logos - An array of logo objects, each containing `url`, `alt`, `lang`, `mode`, and optional `className` and `width`.
+ * @param {string} [lang] - The language code to match logos with. Defaults to the application's default language.
+ * @param {number} [default_width] - The default width of the logo if not specified in the matching logo object.
+ * @param {string} [force_mode] - Forces the mode to either "dark" or "light". If not provided, the mode is determined by the user's system preferences.
+ *
+ * @returns {JSX.Element} The rendered logo image or nothing if no logo is available.
+ */
 const Logo = ({ logos, lang, default_width, force_mode }) => {
   const [darkMode, setDarkMode] = useState(false); // Default to light mode
   const language = lang || config.default_language; // Default to config's default language
@@ -24,16 +35,18 @@ const Logo = ({ logos, lang, default_width, force_mode }) => {
     }
   }, [force_mode]);
 
-  // Handle undefined logos
+  // Handle undefined logos by falling back to config's bottom_logo or an empty array
   const availableLogos = logos || config.bottom_logo || [];
 
   // Find the matching logo based on language and mode
   const matchingLogo = availableLogos.find(
     (logo) =>
+      // Match logo language with the provided language or allow logos without a language
       (logo.lang === language || !logo.lang) &&
+      // Match logo mode based on darkMode or force_mode
       ((force_mode ? force_mode === "dark" : darkMode)
-        ? logo.mode === "dark"
-        : logo.mode === "light"),
+        ? logo.mode === "dark" // Use dark mode logo if darkMode or force_mode is dark
+        : logo.mode === "light"), // Use light mode logo otherwise
   );
 
   // Fallback to the first logo if no match is found
