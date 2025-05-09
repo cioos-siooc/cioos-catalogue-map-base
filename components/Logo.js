6579288d6d9929034438a35f34c71238f83fbf4d
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-05-09 12:09:21 -0400

    add more docs

--- components/Logo.js
+++ components/Logo.js
@@ -6,12 +6,28 @@ import config from "@/app/config.js";
  * A React component that renders a logo based on the provided language, mode, and available logos.
  * It supports automatic detection of dark mode or a forced mode, and selects the appropriate logo accordingly.
  *
+ * @component
  * @param {Object[]} logos - An array of logo objects, each containing `url`, `alt`, `lang`, `mode`, and optional `className` and `width`.
+ * @param {string} [logos[].url] - The URL of the logo image.
+ * @param {string} [logos[].alt] - The alt text for the logo image.
+ * @param {string} [logos[].lang] - The language code associated with the logo.
+ * @param {string} [logos[].mode] - The mode of the logo, either "dark" or "light".
+ * @param {string} [logos[].className] - Optional CSS class name for the logo image.
+ * @param {number} [logos[].width] - Optional width of the logo image.
  * @param {string} [lang] - The language code to match logos with. Defaults to the application's default language.
  * @param {number} [default_width] - The default width of the logo if not specified in the matching logo object.
  * @param {string} [force_mode] - Forces the mode to either "dark" or "light". If not provided, the mode is determined by the user's system preferences.
  *
  * @returns {JSX.Element} The rendered logo image or nothing if no logo is available.
+ *
+ * @example
+ * // Example usage of the Logo component
+ * const logos = [
+ *   { url: "/logo-light.png", alt: "Light Logo", lang: "en", mode: "light" },
+ *   { url: "/logo-dark.png", alt: "Dark Logo", lang: "en", mode: "dark" },
+ * ];
+ *
+ * <Logo logos={logos} lang="en" default_width={100} force_mode="dark" />
  */
 const Logo = ({ logos, lang, default_width, force_mode }) => {
   const [darkMode, setDarkMode] = useState(false); // Default to light mode
