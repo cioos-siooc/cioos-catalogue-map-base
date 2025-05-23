2676382676717fcd417686ee4b0e907db18c26e3
turcotteb <turcotteb@ogsl.ca>
2025-05-23 12:17:26 +0000

    last changes for colors in UI

--- components/ModalAPropos.js
+++ components/ModalAPropos.js
@@ -16,14 +16,15 @@ export default function ModalAPropos(lang) {
 
   return (
     <>
-      <Button
-        outline
-        className="p-2 m-2 uppercase"
-        onClick={() => setOpenModal(true)}
-      >
+      <Button className="p-2 m-2 uppercase" onClick={() => setOpenModal(true)}>
         {t.about}
       </Button>
-      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+      <Modal
+        className="dark:bg-primary-900"
+        dismissible
+        show={openModal}
+        onClose={() => setOpenModal(false)}
+      >
         <ModalHeader>À propos du catalogue cartographique</ModalHeader>
         <ModalBody>
           <p className="space-y-6 text-base leading-relaxed">
