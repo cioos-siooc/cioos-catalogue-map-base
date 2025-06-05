e8c94b829a53af440c95aa9e91874a92ecb95c56
turcotteb <turcotteb@ogsl.ca>
2025-06-05 13:14:56 +0000

    color changes in the multi select

--- components/SelectReact.js
+++ components/SelectReact.js
@@ -51,6 +51,61 @@ export function SelectReactComponent({
             ...base,
             zIndex: 9999,
           }),
+          menu: (base) => ({
+            ...base,
+            backgroundColor:
+              typeof window !== "undefined" &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#0f172a" // Tailwind dark:bg-primary-900
+                : "#fff", // Light mode: white
+            color:
+              typeof window !== "undefined" &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#f3f4f6" // Tailwind gray-100
+                : "#000000", // Light mode: black
+          }),
+          option: (base, state) => ({
+            ...base,
+            backgroundColor: state.isFocused
+              ? typeof window !== "undefined" &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#1e293b" // Tailwind dark:bg-primary-700
+                : "#dbeafe" // Tailwind bg-primary-100
+              : typeof window !== "undefined" &&
+                  window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#0f172a" // Tailwind dark:bg-primary-900
+                : "#fff", // Light mode: white
+            color:
+              typeof window !== "undefined" &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#f3f4f6" // Tailwind gray-100
+                : "#000000", // Light mode: black
+          }),
+          multiValue: (base) => ({
+            ...base,
+            backgroundColor:
+              typeof window !== "undefined" &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#0f172a" // Tailwind dark:bg-primary-900
+                : "#bfdbfe", // Tailwind bg-primary-200
+            color: "#ffffff", // Tailwind text-gray-500
+          }),
+          multiValueLabel: (base) => ({
+            ...base,
+            color:
+              typeof window !== "undefined" &&
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "#ffffff" // Tailwind dark:bg-primary-900
+                : "#000000", // Tailwind bg-primary-200
+          }),
+          multiValueRemove: (base) => ({
+            ...base,
+            color: "#6b7280",
+            ":hover": {
+              backgroundColor: "#1e293b", // Tailwind dark:bg-primary-700
+              color: "#9ca3af", // Tailwind dark:text-gray-400
+            },
+          }),
         }}
       />
     </>
