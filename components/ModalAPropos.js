7c4a788ee9d7c48c1e8ed5a929950721a0e6ba7d
OGSL-BRYAN\turco <turcotteb@ogsl.ca>
2025-06-12 14:19:31 +0000

    Ajout des traductions pour le pop up a propos

--- components/ModalAPropos.js
+++ components/ModalAPropos.js
@@ -24,17 +24,10 @@ export default function ModalAPropos(lang) {
         onClick={() => setOpenModal(true)}
       />
       <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
-        <ModalHeader>À propos du catalogue cartographique</ModalHeader>
+        <ModalHeader>{t.about_header}</ModalHeader>
         <ModalBody>
           <p className="space-y-6 text-base leading-relaxed">
-            Le catalogue cartographique est un outil d’exploration de données
-            qui permet d’afficher les entrées du Catalogue de données de l’OGSL
-            sur une carte, avec la possibilité de filtrer les résultats par
-            variable océanique* tels que la température de surface de l’eau ou
-            le phytoplancton. En cliquant sur les points de données et leur
-            étendue géospatiale, le panneau latéral affiche les informations du
-            jeu de données (titre, description, producteur de donnée et liens
-            pour la page du Catalogue et les fichiers de données).
+            {t.about_text}
           </p>
         </ModalBody>
         <ModalFooter></ModalFooter>
