9c1ae6a73d3667176bbc0a1934e73b1a03edf4f3
OGSL-BRYAN\turco <turcotteb@ogsl.ca>
2025-06-17 12:28:04 +0000

    markdown changes

--- components/DatasetDetails.js
+++ components/DatasetDetails.js
@@ -45,18 +45,6 @@ function Item({ label, value, href, className = "" }) {
   );
 }
 
-function markdownToHtml(markdown) {
-  if (!markdown) return "";
-  return markdown
-    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
-    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-    .replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>")
-    .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
-    .replace(/\*(.*)\*/gim, "<i>$1</i>")
-    .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2">$1</a>')
-    .replace(/\n$/gim, "<br>");
-}
 
 export function DatasetDetails({ dataSetInfo, lang}) {
   const { isDrawerOpen, closeDrawer } = useContext(DrawerContext);
