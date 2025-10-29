1624bfca8469c44080c3e3da27a236ce64123505
Richard Kelly <Richard.Kelly@cioos.ca>
2025-10-29 10:15:57 -0230

    removed debug message

--- components/ModalPages.js
+++ components/ModalPages.js
@@ -17,7 +17,6 @@ export default function ModalPages({ lang }) {
     const page = pages[openKey];
     if (!page) return;
     if (page.markdown_content && page.markdown_content[lang]) {
-      console.log("fetching about page:");
       fetch(basePath + page.markdown_content[lang])
         .then((res) => res.text())
         .then((md) =>
