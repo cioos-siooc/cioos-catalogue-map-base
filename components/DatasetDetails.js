fb91c1994ae1982fd95dacb7cf4395b7f7cbf412
Jessy Barrette <jessy.barrette@users.noreply.gitlab.com>
2025-11-20 10:55:13 -0500

    Update DatasetDetails component title styling for consistency

--- components/DatasetDetails.js
+++ components/DatasetDetails.js
@@ -114,7 +114,7 @@ export function DatasetDetails({ dataSetInfo, lang }) {
             })()}
 
           {/* Dataset Title */}
-          <h2 className="text-base font-extrabold text-black dark:text-white">
+          <h2 className="text-base font-bold text-black dark:text-white">
             {dataSetInfo?.title_translated[lang]}
           </h2>
 
